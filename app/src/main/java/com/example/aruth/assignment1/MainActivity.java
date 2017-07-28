12c2f974d703be02fe43e65fe9bab0dbba16460b
Alexmruth <alexmruth@gmail.com>
2017-07-28 18:55:08 +1000

    Initial commit

---
+++ app/src/main/java/com/example/aruth/assignment1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aruth.assignment1;
+
+import android.icu.util.TimeZone;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timezone_list);
+    }
+
+}
