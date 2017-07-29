0f431204ec5c1f13c04947724d269573fcfe142b
Alexmruth <alexmruth@gmail.com>
2017-07-29 16:26:35 +1000

    further progression

--- app/src/main/java/com/example/aruth/assignment1/MainActivity.java
+++ app/src/main/java/com/example/aruth/assignment1/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.timezone_list);
+        setContentView(R.layout.activity_main);
     }
 
 }
