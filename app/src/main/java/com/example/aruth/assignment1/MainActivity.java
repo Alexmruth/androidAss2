cb1a7a6ea691292072da6f558c540f2b5670e6f5
Alexmruth <alexmruth@gmail.com>
2017-08-02 11:32:24 +1000

    finalizing for submit

--- app/src/main/java/com/example/aruth/assignment1/MainActivity.java
+++ app/src/main/java/com/example/aruth/assignment1/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.aruth.assignment1;
 import android.icu.util.TimeZone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.activity_main);
     }
-
 }
